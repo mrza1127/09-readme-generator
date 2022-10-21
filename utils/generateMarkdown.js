d3d1ff29a2503eff44316191197f9db1293593b2
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-20 21:13:36 -0500

    updated generatemarkdown..

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,95 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
+const renderLicenseBadge = license => {
+  if (!license) {
+    return ''
+  }
+
+  return `
+  ${license} Badge Placeholder` 
+}
+
+// table of contents here
+generateTOC = () => {
+  return `
+  Table of Contents will go here
+  `
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license => {
+  if (!license) {
+    return ''
+  }
+
+  return `
+  [${license}](https://www.google.com)
+  `
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = license => {
+  if (!license) {
+    return ''
+  }
+
+  return `
+  This is covered by the ${renderLicenseLink(license)} license.
+  `
+}
+
+// Questions:
+// name
+// title
+// email
+// github repo link
+// github username 
+// description
+
+// usage
+// test
+// instructions for installation
+// contributors
+// license
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ${generateTOC()}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  Contributions were made by ${data.username} and ${data.contributors}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Contact
+  
+  If you have any further questions, contact ${data.name} at ${data.email}
 
 `;
 }
